5362b582931b3d2d7303d04bd7085cdb1bf4657a
Vladyslav Pavlenko <fixitex0206@gmail.com>
2026-06-27 22:15:20 +0200

    hidden neuron, XOR

--- network.py
+++ network.py
@@ -9,9 +9,9 @@ X = np.array([
 
 y = np.array([
     [0],
-    [0],
-    [0],
-    [1]
+    [1],
+    [1],
+    [0]
 ])
 
 def sigmoid(x):
@@ -24,28 +24,47 @@ def sigmoid_derivative(x):
 np.random.seed(42)
 
 weights = np.random.rand(2, 1)
-bias = np.random.rand(1)
+bias = np.zeros((1, 1))
 
-learning_rate = 0.9  
+hidden_weights = np.random.rand(2, 2)
+hidden_bias = np.zeros((1, 2))
+
+learning_rate = 1.0
 
 # bigger learning rate for faster convergence
 # but can lead to overshooting the correct solution if too high
-epochs = 10000
+epochs = 50000
 
 for epoch in range(epochs):
-    z = X @ weights + bias
+    #-------forward pass-------
+    hidden_z = X @ hidden_weights + hidden_bias
+    hidden_predictions = sigmoid(hidden_z)
+    
+    z = hidden_predictions @ weights + bias
     predictions = sigmoid(z)
     
+    #------loss-------
     error = predictions - y
     loss = np.mean(error ** 2)
     
+    #------backward pass: gradients-------
     d_loss_predictions = 2 * error / len(y)
     d_predictions_z = sigmoid_derivative(z)
     
     d_loss_z = d_loss_predictions * d_predictions_z
     
-    d_weights = X.T @ d_loss_z
+    d_hidden_predictions = d_loss_z @ weights.T
+    d_hidden_z = d_hidden_predictions * sigmoid_derivative(hidden_z)
+    
+    d_hidden_weights = X.T @ d_hidden_z
+    d_hidden_bias = np.sum(d_hidden_z, axis=0, keepdims=True)
+       
+    d_weights = hidden_predictions.T @ d_loss_z
     d_bias = np.sum(d_loss_z)
+    
+    #-------parameters update-------
+    hidden_weights -= learning_rate * d_hidden_weights
+    hidden_bias -= learning_rate * d_hidden_bias
     
     weights -= learning_rate * d_weights
     bias -= learning_rate * d_bias
@@ -53,9 +72,12 @@ for epoch in range(epochs):
     if epoch % 1000 == 0:
         print(f"Epoch {epoch}, Loss: {loss:.6f}")
         
+hidden_predictions = sigmoid(X @ hidden_weights + hidden_bias)
+predictions = sigmoid(hidden_predictions @ weights + bias)
+
 print(f"\nTrained weights: {weights.flatten()}")
 print(f"Trained bias: {bias[0]}")
-print (f"\nPredictions: {sigmoid(X @ weights + bias)}")
+print (f"\nPredictions: \n{predictions}")
 
-final_predictions = (sigmoid(X @ weights + bias) > 0.5).astype(int)
+final_predictions = (predictions > 0.5).astype(int)
 print(f"\nFinal Predictions: {final_predictions.flatten()}")
